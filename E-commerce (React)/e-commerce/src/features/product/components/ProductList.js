a01c56acedb5f72f445cc5736e6bee1db8581eaa
nishanthnoel <nishanthnoel14@gmail.com>
2025-02-17 16:35:14 +0530

    1.39 onChange working code

--- E-commerce (React)/e-commerce/src/features/product/components/ProductList.js
+++ E-commerce (React)/e-commerce/src/features/product/components/ProductList.js
@@ -214,7 +214,9 @@ export default function ProductList() {
                                 <div className="flex h-5 shrink-0 items-center">
                                   <div className="group grid size-4 grid-cols-1">
                                     <input
-                                      defaultChecked={option.value}
+                                      defaultChecked={option.checked}
+                                      defaultValue={option.value}
+                                      // checked={option.checked}
                                       id={`filter-mobile-${section.id}-${optionIdx}`}
                                       name={`${section.id}[]`}
                                       type="checkbox"
@@ -378,6 +380,10 @@ export default function ProductList() {
                                       id={`filter-${section.id}-${optionIdx}`}
                                       name={`${section.id}[]`}
                                       type="checkbox"
+                                      onChange={(e) => {
+                                        console.log(e.target.value); // Log the checkbox state when it's changed
+                                        // dispatch(toggleCheckbox(e.target.checked)); // Dispatch the Redux action
+                                      }}
                                       className="col-start-1 row-start-1 appearance-none rounded-sm border border-gray-300 bg-white checked:border-indigo-600 checked:bg-indigo-600 indeterminate:border-indigo-600 indeterminate:bg-indigo-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 forced-colors:appearance-auto"
                                     />
                                     <svg
